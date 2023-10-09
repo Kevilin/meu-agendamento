1ac6fc9b6a637980605074e835efe179d2076eda
charles.muller <charlesmuller@rede.ulbra.br>
2023-10-08 21:53:31 -0300

    Adicionando tela de agendar

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,18 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import CadastroAgendamento from '../components/CadastroAgendamento/CadastroAgendamento';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle size="large" class="ion-text-center">Agendar</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <CadastroAgendamento />
       </IonContent>
     </IonPage>
   );
